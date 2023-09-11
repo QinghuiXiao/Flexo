fd8618ef017b99c5af23e1aa0a9e136f8345cd32
QinghuiXiao <138593048+QinghuiXiao@users.noreply.github.com>
2023-09-11 09:06:02 +0800

    Delete test directory

--- test/flexoelectricity.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import torch.optim as optim
-import torch
-from torch.utils.data import DataLoader
-from Trainer.model import NeuralNet, MultiVariatePoly
-import time
-torch.autograd.set_detect_anomaly(True)
-torch.manual_seed(128)
