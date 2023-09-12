64db32b03c3d7ac0f3a4638551dfc154d20b8fd9
QinghuiXiao <138593048+QinghuiXiao@users.noreply.github.com>
2023-09-12 17:03:38 +0800

    from xiao

--- Trainer/utils.py
+++ Trainer/utils.py
@@ -1,23 +1,23 @@
-# TODO : write a structure grid generator
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
 #from matplotlib.mlab import griddata
 
 class Plot2D:
-    def Contour2D(nodecoords,sol,savefig=False,figname=''):
+    def Quiver2D(nodecoords, sol, savefig=False, figname=''):
         fig, ax = plt.subplots(constrained_layout=True)
-        x,y=nodecoords[:,0],nodecoords[:,1]
+        x, y=nodecoords[:, 0],nodecoords[:, 1]
         xmin, xmax = np.min(x), np.max(x)
         ymin, ymax = np.min(y), np.max(y)
-        X, Y = np.meshgrid(np.linspace(xmin, xmax, 100), np.linspace(ymin, ymax, 100))
-        Z = griddata(nodecoords, sol, (X, Y))
-        #X,Y=np.meshgrid(x,y)
-        #Z=griddata((x,y),sol,(X,Y),method='linear')
-        cs=plt.contourf(X,Y,Z,cmap=plt.cm.hsv,levels=200)
-        #cs=plt.contourf(X,Y,Z,cmap=plt.cm.viridis,levels=200,antialiased=True,extend='both')
-        #cs=plt.tricontourf(x,y,sol,levels=20,cmap="jet")
+        X, Y = np.meshgrid(np.linspace(xmin, xmax, 30), np.linspace(ymin, ymax, 20))
+        P1 = sol[:, 0]
+        P2 = sol[:, 1]
+        magnitude = np.sqrt(P1**2 + P2**2)
+        P1_normalized = P1 / magnitude
+        P2_normalized = P2 / magnitude
+
+        cs= plt.quiver(X, Y, P1_normalized, P2_normalized, magnitude, angles='xy', scale_units='xy', scale=1, cmap='jet')
+
         fig.colorbar(cs)
         ax.axis('equal')
 
@@ -29,4 +29,20 @@ class Plot2D:
                 fig.savefig('result.jpg',dpi=300,bbox_inches='tight')
                 print('save result to result.jpg')
 
-#Class StrgirdEngine:
+class StrgridEngine:
+    def __init__(self, dimension=2, grid_size=(30, 20)):
+        self.dimension = dimension
+        self.grid_size = grid_size
+
+    def generate_structure_grid(self):
+        # Generate a structured grid of points
+        x_points = torch.linspace(0, 1, self.grid_size[0])
+        y_points = torch.linspace(0, 1, self.grid_size[1])
+
+        # Create grid using meshgrid
+        grid_x, grid_y = torch.meshgrid(x_points, y_points)
+
+        # Flatten the grid
+        flattened_grid = torch.stack((grid_x.reshape(-1), grid_y.reshape(-1)), dim=1)
+
+        return flattened_grid
